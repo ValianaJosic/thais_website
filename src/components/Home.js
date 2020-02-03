c8cb5f46331bfb3777e66ff21254513c4f3a6dbb
Valiana <valiana.josic@gmail.com>
2020-02-03 11:50:16 -0500

    NAV updated CSS

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,15 +7,6 @@ import sample from '../Video.mp4';
 function Home() {
   return (
     <div className="App">
-      <div className="nav-div">
-      {/* <nav className="nav">
-          <ul>
-            <li><a href="index.html" className="nav-page current-page">Home</a></li>
-            <li><a href="projects.html" className="nav-page">Projects</a></li>   
-            <li><a href="contact.html" className="nav-page">Contact</a></li>   
-          </ul>
-      </nav> */}
-      </div>
       <header className="App-header">
    
         <p> 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,10 +5,12 @@ import { NavLink } from 'react-router-dom';
  
 const Navigation = () => {
     return (
-      <div classNam="nav-div">
+      <div className="nav-div">
+          <div className="nav">
           <NavLink to="/"  className="nav-page">Home</NavLink>
           <NavLink to="/about"  className="nav-page">About</NavLink>
           <NavLink to="/contact"  className="nav-page">Contact</NavLink>
+          </div>
        </div>
     );
 }
