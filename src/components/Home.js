919f6abc04fc93eef27344fc5d09ef9536148001
Valiana <valiana.josic@gmail.com>
2020-02-03 11:59:32 -0500

    Profile picture

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../App.css';
 import sample from '../Video.mp4';
-
+import thais from '../thais.jpg'
 
 
 function Home() {
@@ -12,9 +12,12 @@ function Home() {
         <p> 
         </p>
       </header>
+      <div>
+        <img src={thais} alt="thais" className="thais"/>
          <video className='videoTag' autoPlay loop muted>
         <source src={sample} type='video/mp4' />
     </video>
+    </div>
     </div>
   );
 }
