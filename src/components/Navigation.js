a5fdc065e0c7d31a54c4314728f4b4bbfe7f2611
Valiana <valiana.josic@gmail.com>
2020-02-07 19:41:30 -0500

    Changed home link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,8 +8,7 @@ const Navigation = () => {
     return (
       <div className="nav-div">
           <div className="nav">
-              <img src={Logo} alt="logo" className="logo"/>
-          <NavLink to="/"  className="nav-page">Home</NavLink>
+          <NavLink to="/"  className="nav-page"> <img src={Logo} alt="logo" className="logo"/></NavLink>
           <NavLink to="/properties"  className="nav-page">Properties</NavLink>
           <NavLink to="/about"  className="nav-page">About</NavLink>
           <NavLink to="/contact"  className="nav-page">Contact</NavLink>
