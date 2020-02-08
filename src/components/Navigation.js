6c1904ab1e075cf3ec30e428b356842da1904939
Valiana <valiana.josic@gmail.com>
2020-02-07 19:19:14 -0500

    Added Logo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css';
+import Logo from '../logo.svg';
  
 import { NavLink } from 'react-router-dom';
  
@@ -7,6 +8,7 @@ const Navigation = () => {
     return (
       <div className="nav-div">
           <div className="nav">
+              <img src={Logo} alt="logo" className="logo"/>
           <NavLink to="/"  className="nav-page">Home</NavLink>
           <NavLink to="/properties"  className="nav-page">Properties</NavLink>
           <NavLink to="/about"  className="nav-page">About</NavLink>
