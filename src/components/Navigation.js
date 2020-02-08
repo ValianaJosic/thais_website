a7833eba59d666fdc95e93021862ebe4ac4f129b
Valiana <valiana.josic@gmail.com>
2020-02-08 18:57:46 -0500

    NAV Changes flex

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,9 +7,9 @@ import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
     return (
-      <div className="nav-div">
-          <div className="nav">
-          <NavLink to="/"  className="nav-page"> <img src={Logo} alt="logo" className="logo"/></NavLink>
+      <div className="nav nav-div">
+           <NavLink to="/"  className="nav-page"> <img src={Logo} alt="logo" className="logo"/></NavLink>
+          <div>
           <NavLink to="/properties"  className="nav-page">Properties</NavLink>
           <NavLink to="/about"  className="nav-page">About</NavLink>
           <NavLink to="/contact"  className="nav-page">Contact</NavLink>
