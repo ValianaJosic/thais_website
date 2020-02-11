da6bb7aa3e1e32aa11d5a2d493713b228a451181
Valiana <valiana.josic@gmail.com>
2020-02-11 18:32:05 -0500

    Properties image

--- src/pages/Properties.js
+++ src/pages/Properties.js
@@ -1,10 +1,11 @@
 import React from 'react';
  
 const Properties = () => {
+   
     return (
        <div>
-          <h1>About US</h1>
-          <p>About US page body content</p>
+         <img src="https://s3.us-east-2.amazonaws.com/thais.website/officelist1.jpeg" alt="listing" className="office1"/>
+         <p>HELLOOO</p>
        </div>
     );
 }
