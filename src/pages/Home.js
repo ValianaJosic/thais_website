d8553fef2ce663043a78b6c894ac55b18ff7c7dc
Valiana <valiana.josic@gmail.com>
2020-02-15 19:31:01 -0500

    Link rel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,9 +14,9 @@ function Home() {
       </video>
   </div>
     <div class="icon-bar">
-      <a href="https://www.facebook.com/Thais-Reyes-357850721490279/"  target="_blank" rel="noopener" class="facebook"><i class="fa fa-facebook"></i></a>
-      <a href="https://twitter.com/thaisreyes" target="_blank" rel="noopener" class="twitter"><i class="fa fa-twitter"></i></a>
-      <a href="https://www.linkedin.com/in/thais-reyes-%F0%9F%8F%96-43474526" target="_blank" rel="noopener" class="linkedin"><i class="fa fa-linkedin"></i></a>
+      <a href="https://www.facebook.com/Thais-Reyes-357850721490279/"  target="_blank" rel="noopener  noreferrer" class="facebook"><i class="fa fa-facebook"></i></a>
+      <a href="https://twitter.com/thaisreyes" target="_blank" rel="noopener  noreferrer" class="twitter"><i class="fa fa-twitter"></i></a>
+      <a href="https://www.linkedin.com/in/thais-reyes-%F0%9F%8F%96-43474526" target="_blank" rel="noopener  noreferrer" class="linkedin"><i class="fa fa-linkedin"></i></a>
   </div>
     <div className="profile-wrapper">
       <div className="profile">
