bcc83a809600c05838fd555aea51fcb3bd2cc4ad
Valiana <valiana.josic@gmail.com>
2020-02-20 11:34:11 -0500

    Added scroll arrow to home

---
+++ src/pages/Home/Scroll/Scroll.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Scroll.css';
+
+
+
+function Scroll() {
+
+    return (
+        <div className="scroll  bounce">
+            <i class="fa fa-angle-double-down arrow"></i>
+        </div>
+    )
+};
+
+export default Scroll;
