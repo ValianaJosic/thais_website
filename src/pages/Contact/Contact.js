8fcb4ae9cea3d9090546043a68d4693741759e52
Valiana <valiana.josic@gmail.com>
2020-02-21 18:29:42 -0500

    Contact information added

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,8 +1,9 @@
 
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Form, FormGroup, Input, Label, Button } from 'reactstrap';
 import '../Contact/Contact.css';
 // import axios from 'axios';
+
 
 
 class Contact extends Component {
@@ -37,35 +38,45 @@ class Contact extends Component {
 
   render() {
     return (
-      <div className="form">
-        <Form onSubmit={this.handleSubmit} style={{ width: '600px' }}>
-          <FormGroup>
-            <Label for="name">Name:</Label>
-            <Input
-              type="text"
-              name="name"
-              onChange={this.handleChange} />
-          </FormGroup>
+      <Fragment>
+        <p className="contact">Thais Reyes
+      <br></br>
+          Real Estate Advisor
+      <br></br>
+          thais.reyes@compass.com
+      <br></br>
+          M: 786.546.0440
+      </p>
+        <div className="form">
+          <Form onSubmit={this.handleSubmit} style={{ width: '600px' }}>
+            <FormGroup>
+              <Label for="name">Name:</Label>
+              <Input
+                type="text"
+                name="name"
+                onChange={this.handleChange} />
+            </FormGroup>
 
-          <FormGroup>
-            <Label for="email">Email:</Label>
-            <Input
-              type="email"
-              name="email"
-              onChange={this.handleChange} />
-          </FormGroup>
+            <FormGroup>
+              <Label for="email">Email:</Label>
+              <Input
+                type="email"
+                name="email"
+                onChange={this.handleChange} />
+            </FormGroup>
 
-          <FormGroup>
-            <Label for="message">Message:</Label>
-            <Input
-              type="textarea"
-              name="message"
-              onChange={this.handleChange} />
-          </FormGroup>
+            <FormGroup>
+              <Label for="message">Message:</Label>
+              <Input
+                type="textarea"
+                name="message"
+                onChange={this.handleChange} />
+            </FormGroup>
 
-          <Button>Submit</Button>
-        </Form>
-      </div>
+            <Button>Submit</Button>
+          </Form>
+        </div>
+      </Fragment>
     );
   }
 }
