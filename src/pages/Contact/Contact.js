e2f4aeae9eafa9c7ac46a117a37eac027ef4a957
Valiana <valiana.josic@gmail.com>
2020-02-21 18:00:41 -0500

    Working on adding axios

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -2,38 +2,38 @@
 import React, { Component } from 'react';
 import { Form, FormGroup, Input, Label, Button } from 'reactstrap';
 import '../Contact/Contact.css';
-import axios from 'axios';
+// import axios from 'axios';
 
 
 class Contact extends Component {
-  constructor() {
-    super()
+  // constructor() {
+  //   super()
 
-    this.state = {
-      name: '',
-      email: '',
-      message: ''
-    }
+  //   this.state = {
+  //     name: '',
+  //     email: '',
+  //     message: ''
+  //   }
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
+  //   this.handleChange = this.handleChange.bind(this)
+  //   this.handleSubmit = this.handleSubmit.bind(this)
+  // }
 
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value})
-  }
+  // handleChange = e => {
+  //   this.setState({ [e.target.name]: e.target.value})
+  // }
 
-  async handleSubmit(e) {
-    e.preventDefault()
+  // async handleSubmit(e) {
+  //   e.preventDefault()
 
-    const {name, email, message} = this.state
+  //   const {name, email, message} = this.state
 
-    const form = await axios.post('/api/form', {
-      name,
-      email,
-      message
-    })
-  }
+  //   const form = await axios.post('/api/form', {
+  //     name,
+  //     email,
+  //     message
+  //   })
+  // }
 
   render() {
     return (
