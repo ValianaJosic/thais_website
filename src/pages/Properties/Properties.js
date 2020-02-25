f59c6432bde4f875063cda07ae8b31162472a1e1
Valiana <valiana.josic@gmail.com>
2020-02-25 16:49:36 -0500

    Carousel added images and erased comments

--- src/pages/Properties/Properties.js
+++ src/pages/Properties/Properties.js
@@ -16,11 +16,17 @@ const items = [
     src: 'https://s3.us-east-2.amazonaws.com/thais.website/officelist2.jpeg',
   },
   {
+    src: 'https://s3.us-east-2.amazonaws.com/thais.website/officelist1.jpeg',
+  },
+  {
+    src: 'https://s3.us-east-2.amazonaws.com/thais.website/officelist5.jpeg',
+  },
+  {
     src: 'https://s3.us-east-2.amazonaws.com/thais.website/officelist3.jpeg',
   }
 ];
 
-const Example = (props) => {
+const Listings = (props) => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [animating, setAnimating] = useState(false);
 
@@ -69,23 +75,5 @@ const Example = (props) => {
   );
 }
 
-export default Example;
+export default Listings;
 
-
-
-// import React from 'react';
- 
-// const Properties = () => {
-   
-//     return (
-//        <div>
-//          <img src="https://s3.us-east-2.amazonaws.com/thais.website/officelist4.jpeg" alt="listing" className="office1"/>
-//          <img src="https://s3.us-east-2.amazonaws.com/thais.website/officelist2.jpeg" alt="listing" className="office1"/>
-//          <img src="https://s3.us-east-2.amazonaws.com/thais.website/officelist3.jpeg" alt="listing" className="office1"/>
-//          <img src="https://s3.us-east-2.amazonaws.com/thais.website/officelist1.jpeg" alt="listing" className="office1"/>
-//          <img src="https://s3.us-east-2.amazonaws.com/thais.website/officelist5.jpeg" alt="listing" className="office1"/>  
-//        </div>
-//     );
-// }
- 
-// export default Properties;
