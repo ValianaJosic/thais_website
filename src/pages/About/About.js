d0b3467a30713740f7a1e3e8ff03ec31099a3170
Valiana <valiana.josic@gmail.com>
2020-02-26 14:45:01 -0500

    About CSS

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -15,7 +15,8 @@ const About = () => {
         <div className="profile-about">
           <p className="about-page">
             Thais Reyes is an experienced Real Estate advisor, a Venezuelan native, who moved to Miami in 1995, captivated by the beauty of Miami, from its pristine beaches, excellent year round weather, cultural diversity, lifestyle, and ever-growing industries. Thais is adamant about the advantages of owning property in South Florida.
-                <br></br>
+            <br></br>
+            <br></br>
             Thais has a passion for the real estate profession, dedication, attention to detail, strong negotiation skills, efficient real estate transactions process and has successfully closed commercial leases as well as residential real estate transactions.<br></br>
             <br></br>
             She has ample experience with in-house development sales (pre-construction) and has worked in high profile condo development projects such as; Infinity at Brickell, Skyline at Brickell, Grove at Grand Bay in Coconut Grove. In addition, she had exclusively listed a boutique building and sold the entire building in South Beach.
@@ -23,6 +24,7 @@ const About = () => {
             She is proficient in marketing, business networking, presentation skills and is committed to achieving her client's real estate goals. <br></br>
             <br></br>
             Her approach to each real estate transaction is goals-driven and personable. Thais consistently aims to help her clients achieve their property's acquisition or lease goals.
+                <br></br>
                 <br></br>
             Encompassing all markets of experience, from Sunny Isles, Miami Beach, Coconut Grove to Brickell Avenue (Miami's Financial District).
                 <br></br>
@@ -32,7 +34,7 @@ const About = () => {
             <img src={AboutIMG} alt="thais" className="about-img" />
           </div>
         </div>
-        <Contactbtn/>
+        <Contactbtn />
       </div>
     </Fragment>
   );
