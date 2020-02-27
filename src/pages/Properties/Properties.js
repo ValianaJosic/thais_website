deaf3c572ed45a3d3ef44ad949526354b035d0a9
Valiana <valiana.josic@gmail.com>
2020-02-26 22:15:03 -0500

    Added iframe and mock api

--- src/pages/Properties/Properties.js
+++ src/pages/Properties/Properties.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import Listings from '../Properties/Carousel/Carousel';
+// import Listings from '../Properties/Carousel/Carousel';
+// import PersonList from '../Properties/service/service';
+import Iframe from 'react-iframe'
+import '../Properties/Properties.css'
 
 function Home() {
   return (
     <div>
-    <Listings/>
+    {/* <Listings/>
+    <PersonList/> */}
+    <div className="iframe-mock">
+    <Iframe url="https://www.compass.com/homes-for-sale/_map/mapview=25.845208,-80.221764,25.793135,-80.297037/?omniboxAttemptId=0fe60769-741a-4fde-9fd3-008725d82e07"
+        width="100%"
+        height="500px"
+        display="initial"
+        position="relative"/>
+        </div>
     </div>
   );
 }
