c63f6a863626c89a77501eb6fcc278f29cbbf5a1
Valiana <valiana.josic@gmail.com>
2020-03-23 18:16:07 -0400

    added forever

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     USER: 'bobhope2015hope@gmail.com', 
-    PASS: 'tbyiyicllvdipkmj'
+    PASS: 'tbyiyicllvdipkmj',
+    APIKEY:'06d1f4cd8abbb0158dc86eb31079177c'
 }
--- src/pages/Properties/service/service.js
+++ src/pages/Properties/service/service.js
@@ -1,24 +1,17 @@
-import axios from 'axios';
+var request = require("request");
 
-
-axios({
-  "method":"POST  ",
-  "url":"https://zillowdimashirokovv1.p.rapidapi.com/GetSearchResults.htm",
-  "headers":{
-  "content-type":"application/x-www-form-urlencoded",
-  "x-rapidapi-host":"ZillowdimashirokovV1.p.rapidapi.com",
-  "x-rapidapi-key":"445356f5d8msh8d69aea21e1a809p10d900jsnf818d0339fca"
-  },"data":{
-  "rentzestimate":"true",
-  "zws-id":"",
-  "citystatezip":"\"\"",
-  "address":"\"\""
+var options = {
+  method: 'GET',
+  url: 'https://realtor.p.rapidapi.com/properties/detail',
+  qs: {listing_id: '608763437', prop_status: 'for_sale', property_id: '4599450556'},
+  headers: {
+    'x-rapidapi-host': 'realtor.p.rapidapi.com',
+    'x-rapidapi-key': '445356f5d8msh8d69aea21e1a809p10d900jsnf818d0339fca'
   }
-  })
-  .then((response)=>{
-    console.log(response)
-  })
-  .catch((error)=>{
-    console.log(error)
-  })
-  
+};
+
+request(options, function (error, response, body) {
+	if (error) throw new Error(error);
+
+	console.log(body);
+});
