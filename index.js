cddcfbfe9430d1780ab0f81186208266a81bee64
Valiana <valiana.josic@gmail.com>
2020-03-24 12:00:51 -0400

    Changed scripts

--- index.js
+++ index.js
@@ -49,8 +49,10 @@ router.post('/send', (req, res, next) => {
   })
 })
 
+
+const PORT = process.env.PORT
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/', router)
-app.listen(3002)
+app.listen(PORT)
