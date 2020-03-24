ae5e04c7272356cbfb9ea9e1335bb92d2b98ff9e
Valiana <valiana.josic@gmail.com>
2020-03-24 17:25:10 -0400

    added production build

--- server.js
+++ server.js
@@ -49,6 +49,14 @@ router.post('/send', (req, res, next) => {
   })
 })
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 const app = express()
 app.use(cors())
 app.use(express.json())
