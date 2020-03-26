a8fd4d8773056d5d97470a4b543e87dedb8c8b20
Valiana <valiana.josic@gmail.com>
2020-03-26 10:34:47 -0400

    path

--- server.js
+++ server.js
@@ -54,13 +54,3 @@ app.use(cors())
 app.use(express.json())
 app.use('/', router)
 app.listen(process.env.PORT ||3002)
-
-const path = require('path');
-const publicPath = path.join(__dirname, 'public');
-
-app.use(express.static(publicPath));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
-});
-
