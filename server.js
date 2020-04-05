c5b082dde62d6966eae67d7770817c74b14d11bb
Valiana <valiana.josic@gmail.com>
2020-04-04 21:15:20 -0400

    Server url changes

--- server.js
+++ server.js
@@ -49,9 +49,9 @@ router.post('/send', (req, res, next) => {
   })
 })
 
-
+const PORT = process.env.PORT || 3002;
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/', router)
-app.listen(3002)
+app.listen(PORT)
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -20,7 +20,7 @@ class Contact extends React.Component {
     e.preventDefault();
     axios({
       method: "POST",
-      url: "/send",
+      url: process.env.PORT ? "/send" : "http://localhost:3002/send",
       data: this.state,
     }).then((response) => {
       if (response.data.status === 'success') {
