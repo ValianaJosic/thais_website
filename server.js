46cafc2147ee56c8ba02f14833610e3d945d875c
Valiana <valiana.josic@gmail.com>
2020-04-05 13:05:10 -0400

    Procfile

--- server.js
+++ server.js
@@ -49,10 +49,10 @@ router.post('/send', (req, res, next) => {
   })
 })
 
-const port = process.env.PORT || 3002;
+
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/', router)
-app.listen(port)
+app.listen(3002)
 
