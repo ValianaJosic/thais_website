069ac7e296fb397df4987f17534f792282786162
Valiana <valiana.josic@gmail.com>
2020-05-22 10:39:23 -0400

    netlify deploy

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,3 +1,74 @@
+import React, { Fragment } from 'react';
+import { Form, FormGroup, Input, Label, Button } from 'reactstrap';
+import ThaisContact from '../../assets/images/thaiscontact.png';
+import ContactIMG from '../../assets/images/compass.jpeg';
+import '../Contact/Contact.css';
+
+export default class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.submitForm = this.submitForm.bind(this);
+    this.state = {
+      status: ""
+    };
+  }
+
+  render() {
+    const { status } = this.state;
+    return (
+      <Fragment>
+        <div><img className="contact-me" src={ThaisContact} alt="Thais"></img></div>
+        <div className="form">
+        <Form
+          onSubmit={this.submitForm}
+          action="https://formspree.io/xjveybgj"
+          method="POST"
+          style={{ width: '600px' }}
+        >
+          <FormGroup>
+            <Label style={{color: '#3A5C83'}}>Name</Label>
+            <Input type="name" name="name" />
+          </FormGroup>
+          <FormGroup>
+            <Label style={{color: '#3A5C83'}}>Email</Label>
+            <Input type="email" name="email" />
+          </FormGroup>
+          <FormGroup>
+            <Label style={{color: '#3A5C83'}}>Message</Label>
+            <Input type="textarea" name="message" />
+          </FormGroup>
+          {status === "SUCCESS" ? <p style={{color: '#3A5C83'}}>Your message was sent.</p> : <Button  className="submit-btn" style={{backgroundColor: '#6096BA'}}>Submit</Button>}
+          {status === "ERROR" && <p style={{color: '#3A5C83'}}>Ooops! There was an error.</p>}
+        </Form>
+        </div>
+        <div className="img-contact">
+          <img src={ContactIMG} alt="thais" className="contact-img" />
+        </div>
+      </Fragment>
+    );
+  }
+
+  submitForm(ev) {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        this.setState({ status: "SUCCESS" });
+      } else {
+        this.setState({ status: "ERROR" });
+      }
+    };
+    xhr.send(data);
+  }
+}
+
+
 // import React, { Fragment } from 'react';
 // import { Form, FormGroup, Input, Label, Button } from 'reactstrap';
 // import '../Contact/Contact.css';
@@ -92,73 +163,3 @@
 
 // export default Contact;
 
-
-import React, { Fragment } from 'react';
-import { Form, FormGroup, Input, Label, Button } from 'reactstrap';
-import ThaisContact from '../../assets/images/thaiscontact.png';
-import ContactIMG from '../../assets/images/compass.jpeg';
-import '../Contact/Contact.css';
-
-export default class Contact extends React.Component {
-  constructor(props) {
-    super(props);
-    this.submitForm = this.submitForm.bind(this);
-    this.state = {
-      status: ""
-    };
-  }
-
-  render() {
-    const { status } = this.state;
-    return (
-      <Fragment>
-        <div><img className="contact-me" src={ThaisContact} alt="Thais"></img></div>
-        <div className="form">
-        <Form
-          onSubmit={this.submitForm}
-          action="https://formspree.io/xjveybgj"
-          method="POST"
-          style={{ width: '600px' }}
-        >
-          <FormGroup>
-            <Label style={{color: '#3A5C83'}}>Name</Label>
-            <Input type="name" name="name" />
-          </FormGroup>
-          <FormGroup>
-            <Label style={{color: '#3A5C83'}}>Email</Label>
-            <Input type="email" name="email" />
-          </FormGroup>
-          <FormGroup>
-            <Label style={{color: '#3A5C83'}}>Message</Label>
-            <Input type="textarea" name="message" />
-          </FormGroup>
-          {status === "SUCCESS" ? <p style={{color: '#3A5C83'}}>Your message was sent.</p> : <Button  className="submit-btn" style={{backgroundColor: '#6096BA'}}>Submit</Button>}
-          {status === "ERROR" && <p style={{color: '#3A5C83'}}>Ooops! There was an error.</p>}
-        </Form>
-        </div>
-        <div className="img-contact">
-          <img src={ContactIMG} alt="thais" className="contact-img" />
-        </div>
-      </Fragment>
-    );
-  }
-
-  submitForm(ev) {
-    ev.preventDefault();
-    const form = ev.target;
-    const data = new FormData(form);
-    const xhr = new XMLHttpRequest();
-    xhr.open(form.method, form.action);
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState !== XMLHttpRequest.DONE) return;
-      if (xhr.status === 200) {
-        form.reset();
-        this.setState({ status: "SUCCESS" });
-      } else {
-        this.setState({ status: "ERROR" });
-      }
-    };
-    xhr.send(data);
-  }
-}
